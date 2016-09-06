51e184248028a2ab8c95264120e066bfb46958b4
Moik <turangr@gmail.com>
2016-09-06 22:55:40 +0300

    Commit

--- js/daminion.js
+++ js/daminion.js
@@ -1,52 +1,75 @@
+$.ajax({
+	url: "daminion.json",
+	type: "GET",
+	dataType: 'json',
+	cache: false,
+	// async: false,
+	success: function(data) {
+		if(data.number) {
+			var select = $("#license-number");
+			for(var i = 1; i <= data.number; i++) {
+				if(i === 1) {
+					select.append("<option value='" + i + "' selected>" + i + "</option>");
+				} else {
+					select.append("<option value='" + i + "'>" + i + "</option>");
+				}
+			}
+			
+		}
+		if(data.licenses) {
+			var radioElem = $(".radio-section");
+			var radioElemHTML = radioElem.html();
+			radioElem.empty();
+			for(var i = 0; i < data.licenses.length; i++) {
+				radioElem.append(radioElemHTML);
+			}
+			$("input[name=license-plan]").each(function (i) {
+				this.id += i + 1;
+				this.value = data.licenses[i].plan;
+			});
+			$(".license-plan").each(function (i) {
+				$(this).attr("for", "lp" + (i + 1));
+			});
+			$(".license-name").each(function (i) {
+				$(this).append(data.licenses[i].plan);
+			});
+			$(".license-price").each(function (i) {
+				$(this).append(data.licenses[i].price);
+			});
+		}
+
+	},
+	error: function(jqXHR, err) {
+		console.log(jqXHR);
+		console.log(err);
+	}
+});
+
 $(function() {
 
-	$('body').click(function() {
-		console.log("olol");
-		$.ajax({
-			url: "daminion.json",
-			type: "GET",
-			// dataType: 'json',
-			cache: false,
-			success: function(data) {
-				if(data.success) {
-					console.log("ura");
-				} else {
-					console.log("vot blya");
-				}
-				console.log("sdkfjslfjglj");
-				$(".test").html("sadgfklksdhj");
-			},
-			error: function(jqXHR, toto) {
-				console.log(jqXHR);
-				console.log(toto);
-			}
-		});
-	});
 
+var	checked = $("input[name=license-plan]:checked"),
+perLicense = +checked.next().find(".license-price").text(),
+selectedPlan = checked.val(),
+number = +$("#license-number option:selected").val(),
+selectedContainer = $(".selected-plan");
 
+totalCalc(perLicense, number);
+selectedContainer.html(selectedPlan);
 
-	var	checked = $("input[name=license-plan]:checked"),
-	perLicense = +checked.next().find(".license-price").text(),
-	selectedPlan = checked.val(),
-	number = +$("#license-number option:selected").val(),
-	selectedContainer = $(".selected-plan");
-
+$("input[name=license-plan]").change(function() {
+	perLicense = +$(this).next().find(".license-price").text();
 	totalCalc(perLicense, number);
+
+	selectedPlan = $(this).val();
 	selectedContainer.html(selectedPlan);
+});
 
-	$("input[name=license-plan]").change(function() {
-		perLicense = +$(this).next().find(".license-price").text();
-		totalCalc(perLicense, number);
+$("#license-number").change(function() {
+	number = +$("#license-number option:selected").val();
+	totalCalc(perLicense, number);
+});
 
-		selectedPlan = $(this).val();
-		selectedContainer.html(selectedPlan);
-	});
-
-	$("#license-number").change(function() {
-		number = +$("#license-number option:selected").val();
-		totalCalc(perLicense, number);
-	});
-	
 });
 
 
